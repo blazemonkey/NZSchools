30588bde8aeb8a092fb2dac51bb549743ee81bad
Gary <xzblaze@hotmail.com>
2015-07-30 00:30:59 +0800

    [Data] Add the ID Property In Model

---
+++ NZSchools.WebServiceApi/DAL/SchoolInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NZSchools.WebServiceApi.DAL
+{
+    // DropCreateDatabaseIfModelChanges
+    public class SchoolInitializer : DropCreateDatabaseAlways<SchoolContext>
+    {
+
+        public SchoolInitializer()
+        {
+
+        }
+    }
+}
